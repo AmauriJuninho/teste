18df19d13c91f955c2000102928a4fea9bdce9ea
AMAURI DE FARIAS RODRIGUES JUNIOR <amauri.frjunior@senacsp.edu.br>
2023-08-24 11:46:15 -0300

    criai uma virgula

---
+++ aula1.py
@@ -0,0 +1,86 @@
+#Aula 1 - 16/08/2023 Variaveis
+# nome
+
+#nome = "Amauri" # String - caracter
+#sobrenome = 'Junior' # string - caracter
+#altura = 1.80 # float - real
+#idade = 24 #int - inteiro
+
+
+# o type é uma função que mostra p tipo do valor
+#print("nome:", nome, "\nsobrenome:", sobrenome, "\naltura:", altura, "\nidade", idade)
+#print("="*30)
+#print(f"nome: {nome}  \nsobrenome: {sobrenome}  \naltura:  {altura}  \nidade  {idade}")
+
+#=====================================================================================================================
+
+#valor = str(12)
+#nota1 = float(input("digite a primeira nota"))
+#nota2 = float(input("digite a segunda nota"))
+#nota3 = float(input("digite a terceira nota"))
+#nota4 = float(input("digite a quarta nota"))
+
+
+#operadores math
+# + soma
+# - subtração
+# / divisão
+# // divisão inteira - (divide sem mostrar o resultado com o resto)
+# % modulo (entrega o resto)
+# * multiplicação
+# ** potenciação
+
+
+
+#print((nota1+nota2+nota3+nota4)/4)
+
+#====================================================================================================================
+
+
+#nome  = input("digite o seu nome")
+#rg    = input("digite o seu rg")
+#cpf   = input("digite o seu cpf")
+
+#print(" ")
+#print("nome:",nome, "\nrg:",rg, "\ncpf:",cpf,)
+
+
+#=====================================================================================================================
+#AULA 2 - 17/08/2023
+
+
+
+#nome="carlos"
+#bd="carlos"
+
+#numero1=4
+#numero2=6
+
+#if nome==bd:
+    #quando uma infirmaçao esta aqui dentro ela so vai acontecer se
+    #for verdade a condiçao
+    #local
+    #idade=45
+
+    #print("entrou no sistema", nome)
+
+
+
+
+#print("acabei meu sistema", idade)
+
+
+#print("fechou o sistema")
+
+#===================================================================================================================
+
+
+numero1=4
+numero2= input("digite sua idade:")
+
+while not numero2.isdigit()
+    numero2= input("digite sua idade:")
+
+numero2= int(numero2)
+print(numero1+numero2)
+
